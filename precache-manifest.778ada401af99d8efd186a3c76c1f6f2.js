f4a493de520e7086221f0f19d45752b2a0301fb9
Frank <2521647197@qq.com>
2022-02-13 10:23:38 +0800

    deploy

---
+++ precache-manifest.778ada401af99d8efd186a3c76c1f6f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42248cf46c7892b7f38676c4fbb6521c",
+    "url": "/blue-money-website/1.png"
+  },
+  {
+    "revision": "721cfd4b031d8f82f484",
+    "url": "/blue-money-website/css/app.73668042.css"
+  },
+  {
+    "revision": "f2a48118e66ca69c14c349e9c1de2dcd",
+    "url": "/blue-money-website/index.html"
+  },
+  {
+    "revision": "721cfd4b031d8f82f484",
+    "url": "/blue-money-website/js/app.152d6cf0.js"
+  },
+  {
+    "revision": "156623125508d76d2a43",
+    "url": "/blue-money-website/js/chunk-vendors.2c2d4054.js"
+  },
+  {
+    "revision": "3f6be5fe74b5e714859b5c9c30bff794",
+    "url": "/blue-money-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/blue-money-website/robots.txt"
+  }
+]);
